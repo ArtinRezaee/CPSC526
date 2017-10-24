365e3cfb457433147ce882ad90c1040d8c8f4c1b
artin Rezaee <Artin@MacBook-Pro.local>
2017-10-24 16:38:09 -0600

    Server can roughly communicate between a client and a remote server

--- server.py
+++ server.py
@@ -2,9 +2,16 @@ import socketserver
 import socket, threading
 import sys
 
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
     BUFFER_SIZE = 4096
     def handle(self):
+        global address
+        add = address
+        global destPort
+        dst = destPort
+        s = socket.socket()
+        s.connect((add,dst))
         while 1:
             data = self.request.recv(self.BUFFER_SIZE)
             if len(data) == self.BUFFER_SIZE:
@@ -15,14 +22,21 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     			break
             if len(data) == 0:
             		break
-            data = data.decode( "utf-8")
-            self.request.sendall( bytearray( "You said: " + data, "utf-8"))
-            print("%s (%s) wrote: %s" % (self.client_address[0],threading.currentThread().getName(), data.strip()))
+            dataClient = data.decode( "utf-8")
+            dataSrv = s.recv(1024).decode("utf-8")
+            # sending to our client which we are the server to
+            self.request.sendall( bytearray( "My server said: " + dataSrv, "utf-8"))
+            # sending to our server which we are the client to
+            s.sendall(dataClient.encode())
+            print("%s (%s) wrote: %s" % (self.client_address[0],threading.currentThread().getName(), dataClient.strip()))
 
+
+address = ''
+destPort = 0
 if __name__ == "__main__":
     if(len(sys.argv)<5):
         srcPort = int(sys.argv[1])
-        server = sys.argv[2]
+        address = sys.argv[2]
         destPort = int(sys.argv[3])
     else:
         logOption = sys.argv[1]
@@ -30,8 +44,9 @@ if __name__ == "__main__":
         replace = sys.argv[3]
         replaceWith = sys.argv[4]
         srcPort = int(sys.argv[5])
-        server = sys.argv[6]
+        address = sys.argv[6]
         destPort = int(sys.argv[7])
+
 
     HOST = "localhost"
     server = socketserver.ThreadingTCPServer((HOST, srcPort), MyTCPHandler)
