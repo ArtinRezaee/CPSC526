9cbd5e258b68d6e50963c95e572b0bf981fdbeb9
ArtinRezaee <artin.anzabee@gmail.com>
2017-10-29 18:07:33 -0600

    -hex done | needs commenting

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import socket, threading
 import sys
 from collections import deque
 
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
     BUFFER_SIZE = 4096
     threads = []
@@ -32,33 +33,64 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     break
             global logOption
             logs = logOption
+            # Decode clientData to use for input modification
             clientData = data.decode()
+            # if the command option is raw, clean up the input and simply log it on the port forwarding server
             if logs == '-raw':
                 lines = clientData.split('\n')
                 for line in lines:
                     print("---> "+line.strip())
+            elif logs == "-hex":
+                hexCounter = 0
+                remainingChars = ''
+                lines = clientData.split('\n')
+                for line in lines:
+                    nline = "".join(line.split())
+                    if not remainingChars:
+                        remainingChars = nline
+                    else:
+                        remainingChars += '.'+nline
+                formattedLine = ''
+                chars = ''
+                for i in range(0,len(remainingChars)):
+                    if (i%16 == 0 and not i == 0):
+                        print("<--- "+hex(hexCounter)[2:].zfill(8),end='   ')
+                        print(formattedLine+'|'+chars+'|')
+                        hexCounter += 16
+                        formattedLine =''
+                        chars=''
+                    elif i+1 == len(remainingChars):
+                        print("<--- "+hex(hexCounter)[2:].zfill(8),end='   ')
+                        spaces = 70 - int(len(formattedLine+'|'+chars+'|'))
+                        print(formattedLine+' '*spaces+'|'+chars+'|')
+                        hexCounter += 16
+                        formattedLine =''
+                        chars=''
+                    if (i+1)%8 == 0:
+                        formattedLine += hex(ord(remainingChars[i]))[2:].zfill(2)+'   '
+                    else:
+                        formattedLine += hex(ord(remainingChars[i]))[2:].zfill(2)+' '
+                    chars += remainingChars[i]
             # Port forwarding server sends received data from client to its server
             self.s.sendall(data)
 
-    #def forward2Client(self):
-        # self.currentConnections += 1
-        # forwarderId = self.totalConnections
-        # self.totalConnections += 1
-        # self.forwardersReadFlags[forwarderId] = False
-
-        # while 1:
-        #     if not serverData.empty():
-        #         if self.forwardersReadFlags[forwarderId] == False:
-        #             if (self.forwardersReadCounter < self.currentConnections):
-        #                 self.request.sendall( bytearray( "My server said: " + self.serverOutputs[0], "utf-8"))
-        #                 self.forwardersReadFlags[forwarderId] = True
-        #                 self.forwardersReadCounter += 1
-        #             else:
-        #                 self.request.sendall( bytearray( "My server said: " + self.serverOutputs[0], "utf-8"))
-        #                 for forwarderId in self.forwardersReadFlags:
-        #                     forwarderReadFlags[forwarderId] = False
-        #                 self.forwardersReadCounter = 0
-        #                 self.serverOutputs.popleft()
+    def forward2Client(self):
+         self.currentConnections += 1
+         forwarderId = self.totalConnections
+         self.totalConnections += 1
+         self.forwardersReadFlags[forwarderId] = False
+         while 1:
+             if self.serverOutputs:
+                 if self.forwardersReadFlags[forwarderId] == False:
+                     if (self.forwardersReadCounter < self.currentConnections-1):
+                         self.request.sendall( bytearray( "My server said: " + self.serverOutputs[0], "utf-8"))
+                         self.forwardersReadFlags[forwarderId] = True
+                         self.forwardersReadCounter += 1
+                     else:
+                         self.request.sendall( bytearray( "My server said: " + self.serverOutputs[0], "utf-8"))
+                         self.forwardersReadFlags[forwarderId] = False
+                         self.forwardersReadCounter = 0
+                         self.serverOutputs.popleft()
 
         # Port forwarding server sends received data to its client
         
@@ -74,7 +106,38 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                     lines = dataSrv.split('\n')
                     for line in lines:
                         print("<--- "+line.strip())
-                print(3)
+            
+                elif logs == "-hex":
+                    hexCounter = 0
+                    remainingChars = ''
+                    lines = dataSrv.split('\n')
+                    for line in lines:
+                        nline = "".join(line.split())
+                        if not remainingChars:
+                            remainingChars = nline
+                        else:
+                            remainingChars += '.'+nline
+                    formattedLine = ''
+                    chars = ''
+                    for i in range(0,len(remainingChars)):
+                        if (i%16 == 0 and not i == 0):
+                            print("<--- "+hex(hexCounter)[2:].zfill(8),end='   ')
+                            print(formattedLine+'|'+chars+'|')
+                            hexCounter += 16
+                            formattedLine =''
+                            chars=''
+                        elif i+1 == len(remainingChars):
+                            print("<--- "+hex(hexCounter)[2:].zfill(8),end='   ')
+                            spaces = 70 - int(len(formattedLine+'|'+chars+'|'))
+                            print(formattedLine+' '*spaces+'|'+chars+'|')
+                            hexCounter += 16
+                            formattedLine =''
+                            chars=''
+                        if (i+1)%8 == 0:
+                            formattedLine += hex(ord(remainingChars[i]))[2:].zfill(2)+'   '
+                        else:
+                            formattedLine += hex(ord(remainingChars[i]))[2:].zfill(2)+' '
+                        chars += remainingChars[i]
                 self.serverOutputs.append(dataSrv)
             else:
                 break
@@ -98,8 +161,17 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             t = threading.Thread(target = self.client2Server)
             self.threads.append(t)
             t.start()
-
+            
+            c = threading.Thread(target = self.server2Client)
+            self.threads.append(c)
+            c.start()
+            
+            f = threading.Thread(target = self.forward2Client)
+            self.threads.append(f)
+            f.start()
+            
             while 1:
+#                continue
                 if self.serverOutputs:
                     if self.forwardersReadFlags[forwarderId] == False:
                         if (self.forwardersReadCounter < self.currentConnections):
