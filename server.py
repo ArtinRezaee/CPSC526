f6e4363ab96bd5ed61c46ed0f0538bfa4511cf43
ArtinRezaee <artin.anzabee@gmail.com>
2017-10-29 21:24:07 -0600

    added comments

--- server.py
+++ server.py
@@ -3,21 +3,26 @@ import socket, threading
 import sys
 from collections import deque
 
+# Global variables to be used by the threads
 totalConnections = 0
 currentConnections = 0
 serverOutputs = deque()
 forwardersReadCounter = 0
 forwardersReadFlags = {}
 
+# Locks to prevent race conditions
 forwardingLock = threading.Lock()
 setupLock = threading.Lock()
 serverReadLock = threading.Lock()
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
     BUFFER_SIZE = 4096
+    # List of reader threads
     threads = []
+    # socket that forwarding server connects with to the server
     s = socket.socket()
 
+    # Forwarding server sends info to server
     def client2Server(self):
         global s
         while 1:
@@ -40,10 +45,13 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                 lines = clientData.split('\n')
                 for line in lines:
                     print("---> "+line.strip())
+            # if command is hex
             elif logs == "-hex":
                 hexCounter = 0
                 remainingChars = ''
+                # Get each line of data
                 lines = clientData.split('\n')
+                # for each line of data join characters in the same line together or add a . to the first character on line below and join
                 for line in lines:
                     nline = "".join(line.split())
                     if not remainingChars:
@@ -52,6 +60,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                         remainingChars += '.'+nline
                 formattedLine = ''
                 chars = ''
+                # Go through the characters and print every 16 character and format the hex digits based on very 8th character
                 for i in range(0,len(remainingChars)):
                     if (i%16 == 0 and not i == 0):
                         print("---> "+hex(hexCounter)[2:].zfill(8),end='   ')
@@ -74,6 +83,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             # Port forwarding server sends received data from client to its server
             global replaceOption
             replaceOpt = replaceOption
+            # if there is a replace option, loop through the lines of data, find that word and replace it with new word and send it to the client otherwise send the data as is
             if replaceOpt == '-replace':
                 lines = clientData.split('\n')
                 replacedData = ''
@@ -87,6 +97,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             else:
                 self.s.sendall(data)
 
+#Readed threads that send port forwarding data received from the server to the client to handle multiple connections
     def forward2Client(self):
         global totalConnections
         global currentConnections
@@ -97,30 +108,41 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         global totalConnectionsLock
         global currentConnectionsLock
 
-
+        #Acquire the lock to prevent race conditions
         setupLock.acquire()
+        # Number of current connections incremented on every thread creation
         currentConnections += 1
+        # Id of the forwarder on every thread creation
         forwarderId = totalConnections
+        #total connections increamented on every thread connection
         totalConnections += 1
+        # set read flag false for the reader
         forwardersReadFlags[forwarderId] = False
+        # set up the lock
         setupLock.release()
 
         while 1:
             with serverReadLock:
+                # if there is an output from the server
                 if serverOutputs:
+                    # if we have the lock
                     with forwardingLock:
+                        # if we havent read this message before and we are not the last reader
                         if forwardersReadFlags[forwarderId] == False:
                             if (forwardersReadCounter < (currentConnections - 1)):
+                                # send the message to the reader and change the flag and increament the number of read forwarders
                                 self.request.sendall( bytearray(serverOutputs[0], "utf-8"))
                                 forwardersReadFlags[forwarderId] = True
                                 forwardersReadCounter += 1
                             else:
+                                # if we are the last reader, send the message and pop the message from queue
                                 self.request.sendall( bytearray(serverOutputs[0], "utf-8"))
                                 forwardersReadFlags = forwardersReadFlags.fromkeys(forwardersReadFlags, False)
                                 forwardersReadCounter = 0
                                 serverOutputs.popleft()
 
     # Port forwarding server sends received data to its client
+    # All raw, hex, replace, strip, and autoN commands same as above
     def server2Client(self):
         global serverOutputs
         while 1:
@@ -182,18 +204,21 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
                         serverOutputs.append(dataSrv)
             else:
                 break
-
+                    
+    # TCP handler
     def handle(self):
         while 1:
             global address
             add = address
             global destPort
             dst = destPort
+            # connect to the dstport specified by the user
             try:
                 self.s.connect((add,dst))
             except:
                 print("Server is already connected. Continue")
-
+            
+            # start reader and writer threads
             t = threading.Thread(target = self.client2Server)
             self.threads.append(t)
             t.start()
@@ -228,15 +253,18 @@ logOption = ''
 destPort = 0
 
 if __name__ == "__main__":
+    # getting the arguments when server is running with no options
     if(len(sys.argv)<5):
         srcPort = int(sys.argv[1])
         address = sys.argv[2]
         destPort = int(sys.argv[3])
+    # server running with only log options
     elif len(sys.argv)<6:
         logOption = sys.argv[1]
         srcPort = int(sys.argv[2])
         address = sys.argv[3]
         destPort = int(sys.argv[4])
+    # server running with both log options and replace options
     else:
         logOption = sys.argv[1]
         replaceOption = sys.argv[2]
@@ -246,6 +274,7 @@ if __name__ == "__main__":
         address = sys.argv[6]
         destPort = int(sys.argv[7])
 
+    # Creating a multithreaded server using the specified host and source port
     HOST = "localhost"
     server = socketserver.ThreadingTCPServer((HOST, srcPort), MyTCPHandler)
 
