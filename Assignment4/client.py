dff93b02f1c0c4cf6fc95703835189f057113984
sam-ao <sam_ao8@hotmail.com>
2017-11-07 15:20:35 -0700

    authentication on client

--- Assignment4/client.py
+++ Assignment4/client.py
@@ -1,1 +1,13 @@
-temp
+import socket
+import os
+
+if __name__ == "__main__":
+	if(len(sys.argv) == 6):
+		command = sys.argv[1]
+		filename = sys.argv[2]
+		destination = sys.argv[3]
+		cipher = sys.argv[4]
+		key = sys.argv[5]
+
+		
+	else:
