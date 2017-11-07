904fde29e172223e06e0ac26cdeea0f59fff7e92
sam-ao <sam_ao8@hotmail.com>
2017-11-07 15:38:10 -0700

    authentication on client

--- Assignment4/client.py
+++ Assignment4/client.py
@@ -1,5 +1,8 @@
 import socket
 import os
+import random
+import string
+import hashlib
 
 if __name__ == "__main__":
 	if(len(sys.argv) == 6):
@@ -9,5 +12,19 @@ if __name__ == "__main__":
 		cipher = sys.argv[4]
 		key = sys.argv[5]
 
-		
+		host, port = destination.split(':')
+
+		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+			connect.connect((host,port))
+		except:
+		nonce = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(16))
+		client.sendall((cipher + "," + nonce).encode('utf-8'))
+
+		data = client.recv(128).decode('utf-8')
+		auth_data = (data + key).encode('utf-8')
+		hash_auth = hashlib.sha256()
+		hash_auth.update((auth_data))
+		client.sendall(hash_auth.hexdigest().encode('utf-8'))
+		result = client.recv(128).decode('utf-8')
 	else:
