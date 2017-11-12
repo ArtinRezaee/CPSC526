e8b55ff8f889767540bbe3e43860e9f23ccc349b
ArtinRezaee <artin.anzabee@gmail.com>
2017-11-11 21:32:01 -0700

    server progress

--- Assignment4/client.py
+++ Assignment4/client.py
@@ -13,20 +13,21 @@ def send(msg, cipher_type):
     global key, nonce, client
     if(cipher_type == 'null'):
         client.sendall(msg.encode('utf-8'))
-    
-    elif(cipher_type == 'aes128'):
-        pass
-    elif(cipher_type == 'aes256'):
+    else:
+        if(cipher_type == 'aes128'):
+            sess_key = hashlib.sha256((key + nonce + "SK").encode('utf-8')).digest()[:16]
+        elif(cipher_type == 'aes256'):
+            sess_key = hashlib.sha256((key + nonce + "SK").encode('utf-8')).digest()
+
         padder = padding.PKCS7(128).padder()
         padded_msg = padder.update(msg.encode('utf-8')) + padder.finalize()
-        
+
         iv = hashlib.sha256((key + nonce + "IV").encode('utf-8')).digest()[:16]
-        sess_key = hashlib.sha256((key + nonce + "SK").encode('utf-8')).digest()
         backend = default_backend()
         cipher = Cipher(algorithms.AES(sess_key), modes.CBC(iv), backend=backend)
         encryptor = cipher.encryptor()
         encrypted_msg = encryptor.update(padded_msg) + encryptor.finalize()
-        
+
         client.sendall(encrypted_msg)
 
 def recv(size, cipher_type):
@@ -35,11 +36,25 @@ def recv(size, cipher_type):
         msg = client.recv(size).decode('utf-8')
     
     elif(cipher_type == 'aes128'):
-        pass
+        data = client.recv(size)
+#        print(data)
+
+        iv = hashlib.sha256((key + nonce + "IV").encode('utf-8')).digest()[:16]
+        sess_key = hashlib.sha256((key + nonce + "SK").encode('utf-8')).digest()[:16]
+        backend = default_backend()
+        
+        cipher = Cipher(algorithms.AES(sess_key), modes.CBC(iv), backend=backend)
+        decryptor = cipher.decryptor()
+        decrypted_data = decryptor.update(data) + decryptor.finalize()
+        
+        unpadder = padding.PKCS7(128).unpadder()
+#        print("decrypted_data:" + str(decrypted_data))
+        unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+        msg = unpadded_data.decode('utf-8')
     elif(cipher_type == 'aes256'):
         data = client.recv(size)
-        print(data)
-        
+#        print(data)
+
         iv = hashlib.sha256((key + nonce + "IV").encode('utf-8')).digest()[:16]
         sess_key = hashlib.sha256((key + nonce + "SK").encode('utf-8')).digest()
         backend = default_backend()
@@ -49,9 +64,9 @@ def recv(size, cipher_type):
         decrypted_data = decryptor.update(data) + decryptor.finalize()
         
         unpadder = padding.PKCS7(128).unpadder()
-        print("decrypted_data:" + str(decrypted_data))
+#        print("decrypted_data:" + str(decrypted_data))
         unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
-        msg = unpadded_data
+        msg = unpadded_data.decode('utf-8')
     return msg
 
 def encrypt(msg):
@@ -76,12 +91,18 @@ if __name__ == "__main__":
         nonce = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(16))
         client.sendall((cipher + "," + nonce).encode('utf-8'))
 
-        data = recv(128, cipher).decode('utf-8')
+        data = recv(128, cipher)
         #data = client.recv(128).decode('utf-8')
         auth_data = (data + key).encode('utf-8')
         msg = hashlib.sha256(auth_data).hexdigest()
         send(msg, cipher)
         #client.sendall(msg.encode('utf-8'))
+        result = recv(128, cipher)
+        print(result)
+        send(command,cipher)
+        result = recv(128, cipher)
+        print(result)
+        send(filename, cipher)
         result = recv(128, cipher)
         print(result)
 
--- Assignment4/server.py
+++ Assignment4/server.py
@@ -3,39 +3,58 @@ import socket
 import sys
 import hashlib
 import random
+import time
+import os
 import string
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
+from socket import gethostname, gethostbyname
 
 
 def send(msg, cipher_type):
     global key, nonce, client_socket
     if(cipher_type == 'null'):
         client_socket.send(msg.encode('utf-8'))
-    
-    elif(cipher_type == 'aes128'):
-        pass
-    elif(cipher_type == 'aes256'):
+    else:
+        if(cipher_type == 'aes128'):
+            sess_key = hashlib.sha256((key + nonce + "SK").encode('utf-8')).digest()[:16]
+        elif(cipher_type == 'aes256'):
+            sess_key = hashlib.sha256((key + nonce + "SK").encode('utf-8')).digest()
+
         padder = padding.PKCS7(128).padder()
         padded_msg = padder.update(msg.encode('utf-8')) + padder.finalize()
-        
+
         iv = hashlib.sha256((key + nonce + "IV").encode('utf-8')).digest()[:16]
-        sess_key = hashlib.sha256((key + nonce + "SK").encode('utf-8')).digest()
         backend = default_backend()
         cipher = Cipher(algorithms.AES(sess_key), modes.CBC(iv), backend=backend)
         encryptor = cipher.encryptor()
         encrypted_msg = encryptor.update(padded_msg) + encryptor.finalize()
-        
+    
         client_socket.send(encrypted_msg)
+
 
 def recv(size, cipher_type):
     global key, nonce, client_socket
     if(cipher_type == 'null'):
-        msg = client.recv(size).decode('utf-8')
+        msg = client_socket.recv(size).decode('utf-8')
     
     elif(cipher_type == 'aes128'):
-        pass
+        
+        data = client_socket.recv(size)
+        
+        iv = hashlib.sha256((key + nonce + "IV").encode('utf-8')).digest()[:16]
+        sess_key = hashlib.sha256((key + nonce + "SK").encode('utf-8')).digest()[:16]
+        backend = default_backend()
+        
+        cipher = Cipher(algorithms.AES(sess_key), modes.CBC(iv), backend=backend)
+        decryptor = cipher.decryptor()
+        decrypted_data = decryptor.update(data) + decryptor.finalize()
+        
+        unpadder = padding.PKCS7(128).unpadder()
+        unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
+        msg = unpadded_data.decode('utf-8')
+
     elif(cipher_type == 'aes256'):
         data = client_socket.recv(size)
         
@@ -49,7 +68,7 @@ def recv(size, cipher_type):
         
         unpadder = padding.PKCS7(128).unpadder()
         unpadded_data = unpadder.update(decrypted_data) + unpadder.finalize()
-        msg = unpadded_data
+        msg = unpadded_data.decode('utf-8')
     return msg
 
 def encrypt(msg):
@@ -59,13 +78,16 @@ if __name__ == "__main__":
     HOST, PORT, key = "localhost", int(sys.argv[1]), sys.argv[2]
     # Boolan to see if user is logged in
     loggedIn = False;
-    
+    # Boolean to see if server has gotten a command
+    gotCommand = False;
     # Create a socket that can handle nc command
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         # Bind to the host and port and start listening
         server.bind((HOST, PORT))
         server.listen(0)
+        print("Listening on port "+str(PORT))
+        print("Using secret key: "+key)
     except socket.error as e:
         if e.errno == 98:
             print("Port is already in use")
@@ -76,39 +98,54 @@ if __name__ == "__main__":
         data = client_socket.recv(128).decode('utf8').strip()
         cipher,nonce = data.split(',')
         auth_token = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(16))
+        print(time.strftime("%Y-%m-%d %H:%M"),": New Connection from "+ client_socket.getpeername()[0] + " cipher="+cipher)
+        print(time.strftime("%Y-%m-%d %H:%M"),": nonce="+nonce)
         send(auth_token, cipher)
 
         while True:
             # Recieve data from client
             data = recv(128,cipher)
+#            print(data)
             if not loggedIn:
                 hash_auth = hashlib.sha256()
                 hash_auth.update((auth_token+key).encode('utf-8'))
                 # Check user's credintials
-                print(data)
-                if hash_auth.hexdigest() == data.decode('utf-8'):
+                if hash_auth.hexdigest() == data:
                     loggedIn = True
                     # Prompt user to enter a command
-                    send('Welcome to the back-door server\nEnter your commands:\n', cipher)
+                    send('Welcome to the back-door server\n', cipher)
                 else:
                     # Terminate client socket if wrong password is inputted
                     send('Wrong password! bye\n', cipher)
                     client_socket.close()
                     break
+            else:
+                if not gotCommand:
+                    gotCommand = True
+                    command = data
+                    send("OK got your command",cipher)
+                else:
+                    if command == 'read':
+                        print(time.strftime("%Y-%m-%d %H:%M"),": command:"+command+ ", filename:"+data)
+                        send("OK downloading "+data,cipher)
+                    elif command == 'write':
+                        print(time.strftime("%Y-%m-%d %H:%M"),": command:"+command+ ", filename:"+data)
+                        send("OK uploading "+data,cipher)
+    
+
+#                # Interpret user info by matching it with one of the definitions in the variable
+#                args = data.split()
+#                if args[0] == "off":
+#                    do_off(server,client_socket,args)
+#                elif args[0] == "logout":
+#                    do_logout(client_socket,args)
+#                    loggedIn = False
+#                    break
 #                else:
-#                    # Interpret user info by matching it with one of the definitions in the variable
-#                    args = data.split()
-#                    if args[0] == "off":
-#                        do_off(server,client_socket,args)
-#                    elif args[0] == "logout":
-#                        do_logout(client_socket,args)
-#                        loggedIn = False
-#                        break
+#                    if not args[0] in interpretInput:
+#                        client_socket.send(b'\033[91mNo such command\n\033[0m')
 #                    else:
-#                        if not args[0] in interpretInput:
-#                            client_socket.send(b'\033[91mNo such command\n\033[0m')
-#                        else:
-#                            interpretInput[args[0]](client_socket,args)
+#                        interpretInput[args[0]](client_socket,args)
 
     # close clients socket
     client_socket.close()
