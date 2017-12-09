4c5fad6e937e62c238db07c22f2299edb0504d14
ArtinRezaee <artin.anzabee@gmail.com>
2017-12-08 20:38:11 -0700

    complete

--- Assignment6/Controller.py
+++ Assignment6/Controller.py
@@ -7,56 +7,61 @@ import string
 import random
 import select
 
+# Function to send bots messages
 def sendPrivMSG(target,message):
     client.send(('PRIVMSG ' + target + ' :'+message +'\n').encode('utf-8'))
 
+# Function to get the status of each bot
 def do_status(client, args):
     global channel
     global id
-    print(channel)
+    
+    # Send each bot the command status
     sendPrivMSG(channel,'status')
     botList = []
+    
+    # set time out for the socket
     client.settimeout(5)
     while True:
         try:
+            # receive data from bots and add each bot to the bot list
             data = client.recv(1024).decode('utf-8')
-            print("A message from server:", data)
-            print('-------------------------------------------------------')
             lines = data.split('\n')
-            print(lines)
-            print('-------------------------------------------------------')
             for line in lines:
                 if line:
                     if 'PRIVMSG controller' + id in line and line[:4] == ':bot':
                         botList.append(line.split(':')[2].strip())
         except socket.timeout:
             break
+
+    # Report back the number of the found bots
     print('found '+ str(len(botList))+' bots: ', end='')
     for bot in botList:
         print(bot+', ', end='')
     print()
 
 
-
+# Function to command bots to perform the attack
 def do_attack(client, args):
-    print("attack", args[1], args[2])
+    # Send the attack command to all bots
     sendPrivMSG(channel, 'attack '+args[1]+' '+args[2])
     attackList = []
+    
+    # Set time out for the socket
     client.settimeout(5)
     while True:
         try:
+            # Get the number of successful/unsuccessful attacks and add it to the attack list
             data = client.recv(1024).decode('utf-8')
-            print("A message from server:", data)
-            print('-------------------------------------------------------')
             lines = data.split('\n')
-            print(lines)
-            print('-------------------------------------------------------')
             for line in lines:
                 if line:
                     if 'PRIVMSG controller' + id in line and line[:4] == ':bot':
                         attackList.append(line.split(':',2)[2].strip())
         except socket.timeout:
             break
+
+    # Report the number of successful/unsuccessful attacks
     success = 0
     fail = 0
     for attack in attackList:
@@ -67,33 +72,65 @@ def do_attack(client, args):
         print(attack)
     print('Total: '+ str(success) + ' successful, '+ str(fail) +' unsuccessful')
 
-
+# Function to command bots to switch host, port, and channel
 def do_move(client, args):
-    print("move", args[1], args[2], args[3])
+    
+    # send move command to all bots
     sendPrivMSG(channel, 'move '+args[1]+' '+args[2]+' '+args[3])
     moveList = []
+    
+    # set time out for the socket
     client.settimeout(5)
     while True:
         try:
+            # receive the number of bots that have moved successfully and add it the move list
             data = client.recv(1024).decode('utf-8')
-            print("A message from server:", data)
-            print('-------------------------------------------------------')
             lines = data.split('\n')
-            print(lines)
-            print('-------------------------------------------------------')
             for line in lines:
                 if line:
                     if 'PRIVMSG controller' + id in line and line[:4] == ':bot':
-                        attackList.append(line.split(':',2)[2].strip())
+                        moveList.append(line.split(':',2)[2].strip())
         except socket.timeout:
             break
+
+    # report back the number of bots that have successfully moved
     print('Total: '+ str(len(moveList)) + ' successfully moved')
 
+# Function to inform the bots that controller is quitting
 def do_quit(client, args):
+    # send the quit command
+    sendPrivMSG(channel, 'quit')
+    # Quit from the channel
+    client.send(('QUIT\n').encode('utf-8'))
+    # close the socket and exit
     client.close()
+    sys.exit()
 
+# Function to command all bots to shutdown
 def do_shutdown(client, args):
-    print("shutdown the bots")
+    # send the command to all bots
+    sendPrivMSG(channel, 'shutdown')
+    shutdownList = []
+    
+    # set time out
+    client.settimeout(5)
+    while True:
+        try:
+            # receive the number of bots that have shutdown successfully and add it to the shutdown list
+            data = client.recv(1024).decode('utf-8')
+            lines = data.split('\n')
+            for line in lines:
+                if line:
+                    if 'PRIVMSG controller' + id in line and line[:4] == ':bot':
+                        shutdownList.append(line.split(':',2)[2].strip())
+        except socket.timeout:
+            break
+
+    # Report back the number of successful shutdowns
+    success = 0
+    for shutdown in shutdownList:
+        print(shutdown)
+    print('Total: '+ str(len(shutdownList)) + ' disconnected')
 
 
 # Structure so that the program can look into to decide which method to call based on user specified argument
@@ -109,57 +146,51 @@ secret = None
 channel = None
 id = ''
 if __name__ == "__main__":
+    # get the input from command line
     HOST = sys.argv[1]
     srcPort = int(sys.argv[2])
     channel = sys.argv[3][1:]
     secret = sys.argv[4]
     
+    # create a connection to the host at the specified port
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST,srcPort))
+    
     while True:
+        # create a unique NICK for the controller and send it to the IRC server or try again on Nick collision
         id = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(6))
-        print(id)
         client.send(('NICK controller'+id+'\n').encode('utf-8'))
         try:
-            print('in try')
             client.settimeout(4)
             data = client.recv(128).decode('utf-8')
-            print ('IRC said:', data)
             if not data == 'ERR_NICKNAMEINUSE':
                 break
         except socket.timeout:
             break
 
+    # Specify the user
     client.settimeout(None)
     client.send(('USER controller * * :controller test\n').encode('utf-8'))
     data = client.recv(1024).decode('utf-8')
-    print('response: ',data)
     if not '001' in data:
         print('You are not welcome')
         sys.exit()
 
+    # Join the specified channel
     client.send(('JOIN '+channel+'\n').encode('utf-8'))
     data = client.recv(1024).decode('utf-8')
-    print('response: ', data)
+    print(data)
     sendPrivMSG(channel,secret)
 
-    print('going to while')
-
+    # Prompt user to enter commands and interpret those commands accordingly
     while True:
         data = input("Please enter your command:")
         # Interpret user info by matching it with one of the definitions in the variable
         args = data.split()
-        if args[0] == "shutdown":
-            do_shutdown(server,client_socket,args)
-        elif args[0] == "quit":
-            do_quit(client_socket,args)
-            loggedIn = False
-            break
+        if not args[0] in interpretInput:
+            print('No such command\n')
         else:
-            if not args[0] in interpretInput:
-                print('No such command\n')
-            else:
-                interpretInput[args[0]](client,args)
+            interpretInput[args[0]](client,args)
                     
     # close clients socket
     client.close()
--- Assignment6/bot.py
+++ Assignment6/bot.py
@@ -9,71 +9,102 @@ import select
 import errno
 from socket import error as socket_error
 
+# Function to send message to controller
 def sendPrivMSG(target,message):
+    global client
     client.send(('PRIVMSG ' + target + ' :'+message +'\n').encode('utf-8'))
 
-def do_status(client, args, id, controller):
+# Function for each bot to send the controller its name
+def do_status(args, id, controller):
     sendPrivMSG(controller,'bot'+id)
 
-def do_attack(client, args, id, controller):
+# Function for bots to attack a specified host
+def do_attack(args, id, controller):
     global counter
+    
+    # Connect to the specified host at the specified port number
     botClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         botClient.connect((args[1],int(args[2])))
+        
+        # Send a counter and the bots name to the attacked host and increase the counter
         botClient.send((str(counter)+' '+'bot'+id+'\n').encode('utf-8'))
         counter += 1
+        
+        # Report back to the controller
         sendPrivMSG(controller, 'bot'+id+': attack successful')
         botClient.close()
     except socket.error:
         sendPrivMSG(controller, 'bot'+id+': attack unsuccessful, connection refused')
-    print("attack", args[1], args[2])
 
-def do_move(client, args, id, controller):
+# Function for bots to switch server
+def do_move(args, id, controller):
+    global client
+    global authorizedControllers
+
     for _ in range(5):
         try:
+            # Connect to the new server on the specified port or try again if refused
             newClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             newClient.connect((args[1],int(args[2])))
         except socket.error:
             print('Connection refused. Trying again...')
             continue
-        print('Connected.')
+        print('Connected')
         while True:
-            print(id)
+            # send a unique NICK or handle Nick collisions
             newClient.send(('NICK bot'+id+'\n').encode('utf-8'))
             try:
                 newClient.settimeout(4)
                 data = newClient.recv(1024).decode('utf-8')
-                print ('IRC said:', data)
                 if not data == 'ERR_NICKNAMEINUSE':
                     break
             except socket.timeout:
                 break
             id = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(6))
         newClient.settimeout(None)
+        
+        # Create a new user on the new IRC server
         newClient.send(('USER bot * * :Bot BotMacBotFace\n').encode('utf-8'))
         data = newClient.recv(1024).decode('utf-8')
-        print('response: ',data)
         if not '001' in data:
             print('You are not welcome')
             continue
+        
+        # Join the specified channel
         newClient.send(('JOIN '+args[3][1:]+'\n').encode('utf-8'))
         data = newClient.recv(1024).decode('utf-8')
+        print(data)
         if not '331' in data and not '332' in data:
-            print('Could not connect to channel.')
+            print('Could not connect to channel')
             continue
         else:
-            print('Joined new channel.')
+            # Report back to the server
+            print('Joined new channel')
             sendPrivMSG(controller, 'bot'+id+': move successful')
+            
+            # Quit the previous channel and close the previous socket
+            client.send(('JOIN 0' + '\n').encode('utf-8'))
             client.send(('QUIT\n').encode('utf-8'))
-            client.close()
+            client.detach()
+            client = newClient
+            authorizedControllers = []
             return
-    print("bots connect to the new IRC server")
 
-def do_quit(client, args, id, controller):
+# Handle the quit action of the controller by removing the specified controller from the authorized controller list
+def do_quit(args, id, controller):
+    global authorizedControllers
+    authorizedControllers.remove(controller)
+
+# Shutdown the bot
+def do_shutdown(args, id, controller):
+    global client
+    # inform the controller that you are shuttingdown
+    sendPrivMSG(controller, 'bot'+id+': disconnected')
+    # Quit the channel, close the socket and exit
+    client.send(('QUIT\n').encode('utf-8'))
     client.close()
-
-def do_shutdown(client, args, id, controller):
-    print("shutdown the bots")
+    sys.exit()
 
 # Structure so that the program can look into to decide which method to call based on user specified argument
 interpretInput = {
@@ -87,48 +118,49 @@ interpretInput = {
 authorizedControllers = []
 controller = ''
 counter = 0
+client = ''
 
 if __name__ == "__main__":
+    
     # getting the arguments when server is running with no options
     HOST = sys.argv[1]
     srcPort = int(sys.argv[2])
     channel = sys.argv[3][1:]
     secret = sys.argv[4]
     while True:
+        # connect to the specified host on the specified port
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect((HOST,srcPort))
         while True:
+            # generate a unique Nick or try again on Nick collision
             id = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(6))
-            print(id)
             client.send(('NICK bot'+id+'\n').encode('utf-8'))
             try:
-                print('in try')
                 client.settimeout(4)
                 data = client.recv(128).decode('utf-8')
-                print ('IRC said:', data)
                 if not '433' in data:
                     break
             except socket.timeout:
                 break
 
         client.settimeout(None)
+        # create a new user on the IRC server
         client.send(('USER bot * * :Bot BotMacBotFace\n').encode('utf-8'))
         data = client.recv(1024).decode('utf-8')
-        print('response: ',data)
         if not '001' in data:
             print('You are not welcome')
             continue
         
+        # Join the specified channel
         client.send(('JOIN '+channel+'\n').encode('utf-8'))
         data = client.recv(1024).decode('utf-8')
-        print('response: ', data)
 
-        print('going to while')
+
         while True:
+            # Get input from controller and interpret commands accordingly
             data = client.recv(1024).decode('utf-8')
             lines = data.split('\n')
             for line in lines:
-                print("A message from server:", data)
                 messages = line.split(':')
 
                 if len(messages) < 3:
@@ -143,7 +175,7 @@ if __name__ == "__main__":
                     if not args[0] in interpretInput:
                         print("No such command\n")
                     else:
-                        interpretInput[args[0]](client,args,id,controller)
+                        interpretInput[args[0]](args,id,controller)
                 else:
                     if message == secret:
                         print(controller + " authorized")
